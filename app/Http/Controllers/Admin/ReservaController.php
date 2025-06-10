7df54b7c2f2aedd7978fddd38ad50603e79e57fc
msannac <msannac783@g.educaand.es>
2025-06-11 00:57:15 +0200

    segundo cambio

--- app/Http/Controllers/Admin/ReservaController.php
+++ app/Http/Controllers/Admin/ReservaController.php
@@ -51,10 +51,9 @@ class ReservaController extends Controller
     {
             
         $citas = \App\Models\Cita::with('actividad')->orderBy('fecha')->get();
-        return $citas;
-        //$clientes = \App\Models\User::orderBy('name')->get();
-        //$actividades = \App\Models\Actividad::where('activo', true)->orderBy('nombre')->get();
-        //return view('Admin.Reservas.crear', compact('citas', 'clientes', 'actividades'));
+        $clientes = \App\Models\User::orderBy('name')->get();
+        $actividades = \App\Models\Actividad::where('activo', true)->orderBy('nombre')->get();
+        return view('Admin.Reservas.crear', compact('citas', 'clientes', 'actividades'));
     }
 
     // Guarda una nueva reserva
